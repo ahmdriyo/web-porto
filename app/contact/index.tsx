f59d65667f8c019aacd58059cba55eba41742112
ahmdriyo <ahmdriyo.tbn.com>
2025-12-27 08:03:06 +0700

    Fix formatting and update stats in Contact and Stats components

--- app/contact/index.tsx
+++ app/contact/index.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 import { motion, useAnimation } from "framer-motion";
 
 import { FaEnvelope, FaMapMarkedAlt, FaPhoneAlt } from "react-icons/fa";
@@ -23,53 +23,54 @@ const info = [
   {
     icon: <FaMapMarkedAlt />,
     title: "Address",
-    description:
-      "Handil Bakti Kec. Alalak Kab. Barito Kuala Kalimantan Selatan",
+    description: "Banjarmasin, Kalimantan Selatan",
   },
 ];
 
-
 const ContactPage = () => {
   const [formData, setFormData] = useState({
-    firstname: '',
-    lastname: '',
-    email: '',
-    phone: '',
-    message: '',
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    message: "",
   });
-  
-  const handleChange = (e : any) => {
+
+  const handleChange = (e: any) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  
-  const handleSubmit = (e : any) => {
+
+  const handleSubmit = (e: any) => {
     e.preventDefault();
-  
-    emailjs.sendForm(
-      'service_xfkzbb9', 
-      'template_0jgp5yz', 
-      e.target,
-      'lUmiAtPDtKTSSUNS9'
-    ).then(() => {      
-      alert('Your message has been successfully sent!');
-      setFormData({
-        firstname: '',
-        lastname: '',
-        email: '',
-        phone: '',
-        message: '',
-      });
-    }, (error) => {
-      console.log(error.text);
-      alert('Failed to send message. Please try again later.');
-    });
-  
-    
+
+    emailjs
+      .sendForm(
+        "service_xfkzbb9",
+        "template_0jgp5yz",
+        e.target,
+        "lUmiAtPDtKTSSUNS9"
+      )
+      .then(
+        () => {
+          alert("Your message has been successfully sent!");
+          setFormData({
+            firstname: "",
+            lastname: "",
+            email: "",
+            phone: "",
+            message: "",
+          });
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Failed to send message. Please try again later.");
+        }
+      );
   };
   const controls = useAnimation();
   const [ref, inView] = useInView({
-    triggerOnce: true, 
-    threshold: 0.3,    
+    triggerOnce: true,
+    threshold: 0.3,
   });
 
   React.useEffect(() => {
@@ -80,8 +81,8 @@ const ContactPage = () => {
   return (
     <motion.div
       ref={ref}
-      initial={{ opacity: 0, x: -50 }}  
-      animate={controls}                
+      initial={{ opacity: 0, x: -50 }}
+      animate={controls}
       transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
       className="py-6 mt-10"
     >
@@ -89,13 +90,13 @@ const ContactPage = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
-            <form 
+            <form
               className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
               onSubmit={handleSubmit}
             >
               <h3 className="text-4xl text-accent">{`let's contact me`}</h3>
               <p className="text-white/60">
-              Write me some messages or sentences
+                Write me some messages or sentences
               </p>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <Input
